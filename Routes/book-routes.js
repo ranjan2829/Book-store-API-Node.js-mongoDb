3b2ea3d0bd8b176ce1c16cb8b79cd0e7ee5ad0ee
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-11-24 17:19:13 +0530

    NOde js + mongo DB

---
+++ Routes/book-routes.js
@@ -0,0 +1,7 @@
+const express=require("express");
+const router=express.Router();
+router.get("/get",);
+router.get("/get/:id",);
+router.post("/add",);
+router.put("update/:id",);
+router.delete("/delete/:id",);
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT=process.env.PORT|| 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 //connect to DB
 
-connectDB();
+//connectDB();
 app.use(express.json());
 
 app.get("/",(req,res)=>{
